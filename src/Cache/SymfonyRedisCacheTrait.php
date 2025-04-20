c92849e45ef4f1940f497e0c93de6dc26c70213c
trevorpe <t.a.profitt@gmail.com>
2025-04-20 13:22:16 -0300

    fix: extract common constructor logic + fix prefix default

--- src/Cache/SymfonyRedisCacheTrait.php
+++ src/Cache/SymfonyRedisCacheTrait.php
@@ -2,18 +2,36 @@
 
 namespace Trevorpe\LaravelSymfonyCache\Cache;
 
+use Illuminate\Config\Repository;
 use Illuminate\Contracts\Redis\Factory;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 trait SymfonyRedisCacheTrait
 {
+    protected Repository $config;
+
     protected Factory $redis;
 
     protected ?string $prefix = null;
 
     protected ?string $connection = null;
 
+    public function __construct(Repository $config, Factory $redis, ?string $prefix = null, ?string $connection = null)
+    {
+        $this->config = $config;
+        $this->redis = $redis;
+        $this->prefix = $prefix;
+        $this->connection = $connection;
+
+        $this->refreshRedisAdapter();
+    }
+
     abstract protected function createRedisAdapter(): AdapterInterface;
+
+    protected function refreshRedisAdapter(): void
+    {
+        $this->cacheAdapter = $this->createRedisAdapter();
+    }
 
     /**
      * @return mixed|\Redis
@@ -40,18 +58,19 @@ trait SymfonyRedisCacheTrait
     public function setConnection($connection)
     {
         $this->connection = $connection;
-        $this->cacheAdapter = $this->createRedisAdapter();
+        $this->refreshRedisAdapter();
         return $this;
     }
 
     public function getPrefix(): string
     {
-        return $this->prefix ?? '';
+        return $this->prefix ?? $this->config['cache.prefix'];
     }
 
     public function setPrefix(?string $prefix): static
     {
         $this->prefix = $prefix;
+        $this->refreshRedisAdapter();
         return $this;
     }
 }
