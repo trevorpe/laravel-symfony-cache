b4f73e32b46061604dcc1c91618011823152527a
trevorpe <t.a.profitt@gmail.com>
2025-04-20 13:55:37 -0300

    feat: add redis store to trait to handle locks

--- src/Cache/SymfonyRedisCacheTrait.php
+++ src/Cache/SymfonyRedisCacheTrait.php
@@ -2,6 +2,7 @@
 
 namespace Trevorpe\LaravelSymfonyCache\Cache;
 
+use Illuminate\Cache\RedisStore;
 use Illuminate\Config\Repository;
 use Illuminate\Contracts\Redis\Factory;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
@@ -16,6 +17,8 @@ trait SymfonyRedisCacheTrait
 
     protected ?string $connection = null;
 
+    protected RedisStore $redisStore;
+
     public function __construct(Repository $config, Factory $redis, ?string $prefix = null, ?string $connection = null)
     {
         $this->config = $config;
@@ -24,6 +27,8 @@ trait SymfonyRedisCacheTrait
         $this->connection = $connection;
 
         $this->refreshRedisAdapter();
+
+        $this->redisStore = new RedisStore($this->redis, $this->getPrefix(), $this->connection ?? 'default');
     }
 
     abstract protected function createRedisAdapter(): AdapterInterface;
@@ -71,6 +76,55 @@ trait SymfonyRedisCacheTrait
     {
         $this->prefix = $prefix;
         $this->refreshRedisAdapter();
+
+        $this->redisStore->setPrefix($prefix);
+
         return $this;
+    }
+
+    /**
+     * Get the Redis connection instance that should be used to manage locks.
+     *
+     * @return \Illuminate\Redis\Connections\Connection
+     */
+    public function lockConnection()
+    {
+        return $this->redisStore->lockConnection();
+    }
+    /**
+     * Specify the name of the connection that should be used to manage locks.
+     *
+     * @param  string  $connection
+     * @return $this
+     */
+    public function setLockConnection($connection)
+    {
+        $this->redisStore->setLockConnection($connection);
+        return $this;
+    }
+
+    /**
+     * Get a lock instance.
+     *
+     * @param  string  $name
+     * @param  int  $seconds
+     * @param  string|null  $owner
+     * @return \Illuminate\Contracts\Cache\Lock
+     */
+    public function lock($name, $seconds = 0, $owner = null)
+    {
+        return $this->redisStore->lock($name, $seconds, $owner);
+    }
+
+    /**
+     * Restore a lock instance using the owner identifier.
+     *
+     * @param  string  $name
+     * @param  string  $owner
+     * @return \Illuminate\Contracts\Cache\Lock
+     */
+    public function restoreLock($name, $owner)
+    {
+        return $this->redisStore->restoreLock($name, $owner);
     }
 }
